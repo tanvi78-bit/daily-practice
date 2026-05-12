cb9e4a09173c0d650a23d6ad9442290fe0839df3
tanvi78-bit <tanvitiwari863@gmail.com>
2026-05-13 00:34:57 +0530

    changes made

--- DDMMYYYY.java
+++ DDMMYYYY.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
+
 public class DDMMYYYY {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0);
-        String S=sc.nextLine();
-        int first= IntegerParseINt(S.substring())
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            String s = scanner.next();
+           int x = (s.charAt(0) - '0') * 10 + (s.charAt(1) - '0');
+            int y = (s.charAt(3) - '0') * 10 + (s.charAt(4) - '0');
+            if (x <= 12 && y <= 12) {
+                System.out.println("BOTH");
+            } else if (y <= 12) {
+                System.out.println("DD/MM/YYYY");
+            } else {
+                System.out.println("MM/DD/YYYY");
+            }
+
+        }
+        scanner.close();
     }
 }
