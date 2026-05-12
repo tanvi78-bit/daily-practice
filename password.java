e6c47dd2daf8c6e58024ba81590e4f62c488e45e
tanvi78-bit <tanvitiwari863@gmail.com>
2026-05-13 00:31:11 +0530

    code corrected

--- password.java
+++ password.java
@@ -1,10 +1,19 @@
 import java.util.*;
 import java.util.regex.*;
+
 public class password {
     public static void main(String[] args) {
-        Scanner sc=new Scanner (System.in);
-        String s=sc.nextLine();
-        String regex="^(?=.*[a-z](?=.*[A-Z](?=.*[0-9](?=.*[@#%&?]).{10,}$)";
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        while (T-- > 0) {
+
+            String s = sc.nextLine();
+
+            String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#%&?]).{10,}$";
 
             if (Pattern.matches(regex, s)) {
                 System.out.println("YES");
@@ -12,6 +21,6 @@ public class password {
                 System.out.println("NO");
             }
         }
-        
+        sc.close();
     }
-
+}
