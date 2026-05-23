0fe75562ea0e8dba07be3109b25421ff2d71300b
tanvi78-bit <tanvitiwari863@gmail.com>
2026-05-23 10:27:20 +0530

    using user input

--- Duplicateremove.java
+++ Duplicateremove.java
@@ -1,4 +1,4 @@
-public class Duplicateremove {
+/*public class Duplicateremove {
     public static void main(String[] args) {
 
         String str = "programming";
@@ -15,5 +15,24 @@ public class Duplicateremove {
         System.out.println("Original String: " + str);
         System.out.println("After Removing Duplicates: " + result);
     }
-}
+}*/
 
+//or
+
+import java.util.Scanner;
+public class Duplicateremove{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str=sc.nextLine();
+        String result="";
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(result.indexOf(ch)==-1){
+            result=result + ch;
+            }
+        }
+        System.out.println("String after removing duplicates: "+ result);
+        sc.close();
+    }
+}
