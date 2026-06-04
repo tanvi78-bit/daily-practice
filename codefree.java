a530e055de78b80cca6963cee7d5a74889f2e6db
tanvi78-bit <tanvitiwari863@gmail.com>
2026-06-04 22:38:53 +0530

    code on 04/06/26

---
+++ codefree.java
@@ -0,0 +1,12 @@
+public class codefree {
+    public static void main(String[] args) {
+
+        // Declare and initialize an array
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Print all elements
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
