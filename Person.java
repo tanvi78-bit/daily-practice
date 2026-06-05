390be9bf338e3c94fe15a834e4b31c83ad021e99
tanvi78-bit <tanvitiwari863@gmail.com>
2026-06-05 23:13:58 +0530

    code on 05/06/26

---
+++ Person.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class details {
+    String name;
+    int age;
+    String city;
+
+    // Method to accept details
+    void getDetails() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Age: ");
+        age = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        System.out.print("Enter City: ");
+        city = sc.nextLine();
+        sc.close();
+    }
+
+    // Method to display details
+    void displayDetails() {
+        System.out.println("\nPerson Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+    }
+}
+
+public class Person {
+    public static void main(String[] args) {
+        details p = new details();
+
+        p.getDetails();
+        p.displayDetails();
+    }
+    }
