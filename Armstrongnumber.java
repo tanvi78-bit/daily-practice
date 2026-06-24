5cacc60d07198179da7950ec59cf5dbd0a38e2ec
tanvi78-bit <tanvitiwari863@gmail.com>
2026-06-24 20:25:43 +0530

    code on 24\06\26

---
+++ Armstrongnumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Armstrongnumber{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num=sc.nextInt();
+        int original=num;
+        int sum=0;
+        int digits=String.valueOf(num).length();
+        while(num>0){
+            int digit=num%10;
+            sum +=Math.pow(digit, digits);
+            num /=10;
+        }
+        if(sum==original){
+            System.out.println(original + " is Armstrong Number");
+        }else{
+            System.out.println(original + " is not a Armstrong Number");
+        }
+        sc.close();
+    }
+}
