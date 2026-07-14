74365f6a318be9c7a2953117e80e23a6d55cf1f1
tanvi78-bit <tanvitiwari863@gmail.com>
2026-07-14 11:50:56 +0530

    code on 14/07/26

---
+++ AkhbaarPrinting.java
@@ -0,0 +1,33 @@
+class PrintingTask extends Thread {
+    String document;
+
+    PrintingTask(String document) {
+        this.document = document;
+    }
+
+    public void run() {
+        System.out.println("Printing: " + document);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        System.out.println(document + " printed successfully.");
+    }
+}
+
+public class AkhbaarPrinting{
+    public static void main(String[] args) {
+
+        Thread t1 = new PrintingTask("Dainik Bharat");
+        Thread t2 = new PrintingTask("Navbharat");
+        Thread t3 = new PrintingTask("Patrika");
+        Thread t4 = new PrintingTask("Hindustan Times");
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
+
